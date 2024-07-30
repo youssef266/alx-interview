4c2bbf8aa880b775b12eb96f72be44c13b1c368c
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-07-30 11:20:41 +0300

    solved

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Island Perimeter
+"""
+
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid
+    """
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
